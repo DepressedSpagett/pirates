8513431e59ebe01fe809eb190bb75964da547829
jvecc006 <jvecc006@plattsburgh.edu>
2024-05-09 12:42:33 -0400

    kraken encounter code

--- game/events/kraken_encounter.py
+++ game/events/kraken_encounter.py
@@ -1,30 +1,34 @@
 from game.display import announce, menu
-import game.superclasses as superclasses
-import game.items as items 
 from game.context import Context
-import game.config as config
+from game.combat import Combat
+# from game.player import Player 
+from game.crewmate import CrewMate  
 import random
 
 class Kraken:
     def __init__(self):
         # Constructor for Kraken class
-        pass  # Placeholder for actual implementation
+        self.health = 100
+
+    def take_damage(self, damage):
+        self.health -= damage
 
 class KrakenEncounter(Context):
-    def __init__(self):
+    def __init__(self, player):
         super().__init__()
-        self.alive = True
-        self.player_health = 100
-        self.crew = [CrewMate() for _ in range(4)]  # Assuming there are 4 crew members
+        self.name = "kraken"
         self.kraken = Kraken()
+        self.alive = True
+        self.player = player
+        self.combat = Combat([CrewMate() for _ in range(random.randint(3, 6))])
 
     def encounter(self):
         announce("You encounter a massive kraken in the depths of the sea.")
-        while self.alive and self.player_health > 0 and self.kraken.health > 0:
+        while self.alive and self.player.gameInProgress and self.kraken.health > 0:
             self.player_action()
 
     def player_action(self):
-        action = menu(["Attack with cannons", "Attack with harpoons", "Defend", "Sail away"])
+        action = menu(["Attack with cannons", "Attack with harpoons", "Defend", "Sail away", "Befriend"])
         if action == 1:
             self.attack_with_cannons()
         elif action == 2:
@@ -33,6 +37,8 @@ class KrakenEncounter(Context):
             self.defend()
         elif action == 4:
             self.sail_away()
+        elif action == 5:
+            self.befriend()
 
     def attack_with_cannons(self):
         announce("You fire the ship's cannons at the kraken!")
@@ -57,145 +63,39 @@ class KrakenEncounter(Context):
             self.kraken_attack()
 
     def defend(self):
-        announce("You take a defensive stance, preparing for the kraken's next move.")
-        self.kraken_attack()
+        announce("The crew braces for the kraken's attack, preparing to defend themselves.")
+        # Perform defensive actions using Combat module
+        self.combat.defend()
+        announce("The crew successfully defends against the kraken's attack!")
+        self.kraken_attack()  # Proceed with the kraken's attack
 
     def sail_away(self):
         announce("You manage to narrowly escape the kraken's grasp and continue your journey.")
         self.alive = False
+    
+    def befriend(self):
+        if random.choice([True, False]):  # 50/50 chance of success
+            announce("You successfully befriend the kraken!")
+            # reward logic here
+            food_reward = random.randint(50, 100)  # Random food supply between 50 and 100 units
+            self.player.get_ship().add_food(food_reward)  # Add food reward to the ship
+            announce("The kraken chirps happily and gives your crew some supplies before swimming away.")
+            announce(f"You received {food_reward} units of food.")
+            self.alive = False
+        else:
+            announce("Your attempt to befriend the kraken fails. It seems unwilling to cooperate.")
+            self.kraken_attack()
 
     def kraken_attack(self):
         kraken_attack_power = random.randint(15, 25)
-        self.player_health -= kraken_attack_power
-        announce(f"The kraken attacks you, dealing {kraken_attack_power} damage!")
-        if self.player_health <= 0:
-            announce("The kraken overwhelms you. You have been defeated.")
-            self.alive = False
+        # Select a crew member to get injured
+        injured_crew_member = random.choice(self.player.get_pirates())
+        injured_crew_member.health -= kraken_attack_power
+        announce(f"The kraken attacks {injured_crew_member.get_name()}, dealing {kraken_attack_power} damage!")
+        # Check if the crew member is killed
+        if injured_crew_member.health <= 0:
+            announce(f"{injured_crew_member.get_name()} has been killed by the kraken!")
+            self.player.cleanup_pirates()  # Clean up the crew members
         else:
-            announce(f"Your health: {self.player_health}. Kraken's health: {self.kraken.health}.")
-
-class CrewMate(superclasses.CombatCritter):
-    def __init__(self):
-        super().__init__(random.choice(['Anne', 'Bartholomew', 'Benjamin', 'Po', 'Eliza', 'Edward', 'Grace', 'Henry', 'Mary', 'Paulsgrave', 'Jack', 'Turgut', 'William', 'Sayyida', 'Emanuel', 'Peter', 'Richard', 'Yang']), 100, 100 + random.randrange(-20, 21))
-        self.skills = {
-            "brawling": random.randrange(10, 101),
-            "swords": random.randrange(10, 101),
-            "melee": random.randrange(10, 101),
-            "guns": random.randrange(10, 101),
-            "cannons": random.randrange(10, 101),
-            "swimming": random.randrange(10, 101)
-        }
-        self.items = [items.Cutlass(), items.Flintlock()]
-        self.powder = 32
-        self.sick = False
-        self.lucky = False
-
-    def start_day(self, ship):
-        ship.take_food(self.get_hunger())
-        if self.sick:
-            self.inflict_damage(1, "Died of their illness")
-            if self.health <= 0:
-                announce(f"{self.name} has died of their illness!")
-        elif self.hurtToday:
-            self.hurtToday = False
-        elif self.health < 100:
-            if self.health >= 75:
-                self.health += random.randint(1, 10)
-            elif self.health >= 50:
-                self.health += random.randint(1, 6)
-            elif self.health >= 25:
-                self.health += random.randint(1, 4)
-            else:
-                self.health += 1
-            if self.health > 100:
-                self.health = 100
-        self.start_turn()
-
-    def start_turn(self):
-        self.reload()
-
-    def end_day(self):
-        if self.sick:
-            if self.isLucky() or random.randint(1, 10) == 1:
-                self.sick = False
-        self.lucky = False
-
-    def process_verb(self, verb, cmd_list, nouns):
-        if verb == "equip":
-            if len(cmd_list) > 1:
-                for i, item in enumerate(config.the_player.inventory):
-                    if item.name == cmd_list[1]:
-                        found = config.the_player.inventory.pop(i)
-                        self.items.append(found)
-                        self.items.sort()
-                        break
-            else:
-                announce("Equip what?")
-        elif verb == "unequip":
-            if len(cmd_list) > 1:
-                for i, item in enumerate(self.items):
-                    if item.name == cmd_list[1]:
-                        found = self.items.pop(i)
-                        config.the_player.inventory.append(found)
-                        config.the_player.inventory.sort()
-                        break
-            else:
-                announce("Unequip what?")
-        elif verb == "inventory":
-            self.print_inventory()
-        elif verb == "restock":
-            if config.the_player.location != config.the_player.ship:
-                announce("Powder and shot can only be restocked on the ship!")
-            else:
-                self.restock()
-        elif verb == "skills":
-            self.print_skills()
-        else:
-            print(f"{self.name} doesn't know how to {verb}")
-
-    def print_inventory(self):
-            for item in self.items:
-                print(item)
-            print()
-
-    def restock(self):
-        restock_needed = 32 - self.powder
-        if config.the_player.powder > restock_needed:
-            self.powder += restock_needed
-            config.the_player.powder -= restock_needed
-        else:
-            self.powder += config.the_player.powder
-            config.the_player.powder = 0
-        if restock_needed == 0:
-            announce(f"{self.name} doesn't need a restock!")
-        elif config.the_player.powder == 0:
-            if restock_needed < (32 - self.powder):
-                announce(f"{self.name} takes the last powder!")
-            else:
-                announce(f"{self.name} reports that the ship is out of powder!")
-        else:
-            announce(f"{self.name} restocks their powder and shot!")
-
-    def reload(self):
-        for item in self.items:
-            item.recharge(self)
-
-    def getAttacks(self):
-        for defendee in self.defendees:
-            defendee.removeDefender(self)
-        self.defendees = []
-        options = []
-        for item in self.items:
-            attack_list = item.getAttacks(self)
-            if len(attack_list) > 0:
-                for putative_attack in attack_list:
-                    if putative_attack not in options:
-                        options.append(putative_attack)
-        if "brawling" in self.skills.keys():
-            options.append(superclasses.CombatAction("punch", superclasses.Attack("punch", "punches", self.skills["brawling"], (1, 11), False), self))
-        options.append(superclasses.CombatAction("defend", superclasses.Defend("defend", "defends"), self))
-        return options
-# Main code
-if __name__ == "__main__":
-    kraken_encounter = KrakenEncounter()
-    kraken_encounter.encounter()
+            announce(f"{injured_crew_member.get_name()} survives the kraken's attack with {injured_crew_member.health} health remaining.")
+            announce(f"Kraken's health: {self.kraken.health}.")
--- game/locations/Kraggy_Rock_Island.py
+++ game/locations/Kraggy_Rock_Island.py
@@ -1,7 +1,9 @@
+from game.display import announce, menu
 from game import location
 import game.config as config
 from game.display import announce
 from game.events import *
+import random
 
 class KraggyRockIsland(location.Location):
     def __init__(self, x, y, w):
@@ -13,9 +15,9 @@ class KraggyRockIsland(location.Location):
         self.locations = {}
         self.locations["beach"] = self.starting_location
         self.locations["cave"] = Cave(self)
-        self.locations["rocks"] = Rocks(self)
+        self.locations["cliffs"] = Cliffs(self)
         self.locations["fire_pit"] = FirePit(self)
-        self.locations["strange_boulder"] = StrangeBoulder(self)
+        self.starting_location.locations["strange_boulder"] = StrangeBoulder(self.starting_location)
 
     def enter(self, ship):
         print("Arrived at Kraggy Rock Island.")
@@ -33,6 +35,7 @@ class RockyBeach(location.SubLocation):
         self.verbs['south'] = self
         self.verbs['east'] = self
         self.verbs['west'] = self
+        self.verbs['explore_beach'] = self
 
     def enter(self):
         announce("You've made landfall at Kraggy Rock Island and landed on a rocky beach.")
@@ -46,6 +49,8 @@ class RockyBeach(location.SubLocation):
             config.the_player.next_loc = self.main_location.locations["cave"]
         elif verb in ["east", "west"]:
             announce("You walk all the way around the island on the beach. There is nothing but rocks.")
+        elif verb == "explore_beach":
+            config.the_player.next_loc = self.locations["strange_boulder"]
 
 class Cave(location.SubLocation):
     def __init__(self, m):
@@ -55,23 +60,99 @@ class Cave(location.SubLocation):
 
     def enter(self):
         announce("You enter a dark cave. The air is thick with mystery.")
+        self.explore_cave()
+
+    def explore_cave(self):
+        announce("You find yourself in a maze-like cave.")
+        while True:
+            direction = menu(["Go left", "Go right", "Go straight", "Exit"])
+            if direction == 4:
+                break
+            else:
+                self.traverse(direction)
+
+        self.find_kraken_egg()
+
+    def traverse(self, direction):
+        roll = random.randint(1, 6)  # Roll a dice
+        if roll > 3:  # Roll above 3, no dead end
+            announce("You continue your journey through the cave.")
+        else:
+            announce("You hit a dead end. Time to backtrack.")
+
+    def find_kraken_egg(self):
+        announce("After navigating through the maze, you find a section of the cave with a nest.")
+        announce("You cautiously approach and discover a kraken egg resting in the nest.")
+        self.take_kraken_egg()
+
+    def take_kraken_egg(self):
+        decision = menu(["Take the kraken egg", "Leave it"])
+        if decision == 1:
+            announce("You carefully take the kraken egg.")
+            # Add any rewards or consequences for taking the egg
+            config.the_player.inventory.append("Kraken Egg")
+        else:
+            announce("You decide to leave the kraken egg undisturbed.")
 
     def process_verb(self, verb, cmd_list, nouns):
         if verb == "exit":
             config.the_player.next_loc = self.main_location.locations["beach"]
 
-class Rocks(location.SubLocation):
+
+class Cliffs(location.SubLocation):
     def __init__(self, m):
         super().__init__(m)
-        self.name = "Rocks"
-        self.verbs['talk'] = self
+        self.name = "Cliffs"
+        self.verbs['listen'] = self
 
     def enter(self):
         announce("You find yourself surrounded by sharp cliffs and rugged edges.")
 
     def process_verb(self, verb, cmd_list, nouns):
-        if verb == "talk":
+        if verb == "listen":
             announce("You hear faint chanting in the distance.")
+            self.encounter_rock_sage()
+
+    def encounter_rock_sage(self):
+        announce("Following the sound of chanting, you come across a mysterious old sage.")
+        announce("The sage beckons you closer and presents you with three riddles.")
+
+        correct_answers = 0
+        for _ in range(3):
+            riddle = self.get_riddle()
+            answer = input(f"Sage's Riddle: {riddle} - Your answer: ")
+            if self.check_answer(riddle, answer):
+                announce("The sage nods in approval.")
+                correct_answers += 1
+            else:
+                announce("The sage shakes his head.")
+        
+        if correct_answers >= 2:
+            announce("Impressed by your wisdom, the sage rewards you with a diamond.")
+            config.the_player.inventory.append("Diamond")
+        else:
+            announce("The sage looks disappointed. You leave without a reward.")
+
+    def get_riddle(self):
+        # Define a list of riddles
+        riddles = [
+            "What has a head, a tail, is brown, and has no legs?",
+            "I'm tall when I'm young, and I'm short when I'm old. What am I?",
+            "The more you take, the more you leave behind. What am I?"
+        ]
+        # Randomly select a riddle from the list
+        return random.choice(riddles)
+
+    def check_answer(self, riddle, answer):
+        # Define a dictionary of correct answers for each riddle
+        correct_answers = {
+            "What has a head, a tail, is brown, and has no legs?": "Penny",
+            "I'm tall when I'm young, and I'm short when I'm old. What am I?": "Candle",
+            "The more you take, the more you leave behind. What am I?": "Footsteps"
+        }
+        # Check if the provided answer matches the correct answer
+        return answer.lower() == correct_answers.get(riddle.lower(), "").lower()
+
 
 class FirePit(location.SubLocation):
     def __init__(self, m):
@@ -84,18 +165,69 @@ class FirePit(location.SubLocation):
 
     def process_verb(self, verb, cmd_list, nouns):
         if verb == "smoke":
-            announce("You follow the smoke signal and encounter a mysterious figure.")
+            announce("You follow the smoke signal and encounter a towering rock golem.")
+            self.encounter_grill_master()
 
-class StrangeBoulder(location.SubLocation):
+    def encounter_grill_master(self):
+        announce("Before you stands the legendary Grill Master, a rock golem known for its BBQ skills.")
+        announce("The Grill Master offers you free rewards for your journey.")
+        kraken_egg = "Kraken Egg" in config.the_player.inventory
+        diamond = "Diamond" in config.the_player.inventory
+
+        if kraken_egg or diamond:
+            announce("The Grill Master notices your special items and offers you a special reward.")
+            self.reward_special()
+        else:
+            self.reward_normal()
+
+    def reward_normal(self):
+        announce("The Grill Master serves you a delicious BBQ feast.")
+        # Provide normal rewards here
+
+    def reward_special(self):
+        announce("The Grill Master is impressed by your special items.")
+        if "Kraken Egg" in config.the_player.inventory:
+            announce("You present the Grill Master with the Kraken Egg.")
+            announce("In return, the Grill Master grants you a special recipe for Kraken Egg BBQ.")
+            # Provide special reward for Kraken Egg
+        if "Diamond" in config.the_player.inventory:
+            announce("You show the Grill Master the Diamond.")
+            announce("The Grill Master crafts you a magical BBQ utensil.")
+            # Provide special reward for Diamond
+
+
+class StrangeBoulder(location.Location):
     def __init__(self, m):
-        super().__init__(m)
+        super().__init__(0, 0, m.world)
         self.name = "Strange Boulder"
         self.verbs['defeat'] = self
+        self.kaptain_kragg_defeated = False
 
     def enter(self):
         announce("You spot a strange boulder with boots sticking out from beneath it.")
+        if not self.kaptain_kragg_defeated:
+            announce("As you approach, the ghostly figure of Kaptain Kragg emerges from the boulder.")
 
     def process_verb(self, verb, cmd_list, nouns):
         if verb == "defeat":
-            announce("You prepare to face the ghost of Kaptain Kragg.")
+            if not self.kaptain_kragg_defeated:
+                self.defeat_kaptain_kragg()
+            else:
+                announce("You've already defeated Kaptain Kragg and claimed the treasure.")
+        else:
+            announce("You need to defeat Kaptain Kragg to claim the treasure.")
+
+    def defeat_kaptain_kragg(self):
+        announce("You engage in a fierce battle with the ghostly Kaptain Kragg.")
+        # Perform battle logic here
+        if True:  # Replace with actual battle outcome logic
+            announce("You emerge victorious over Kaptain Kragg!")
+            self.kaptain_kragg_defeated = True
+            announce("Kaptain Kragg's treasure, phasing through the boulder, is now accessible.")
+            self.reward_treasure()
+
+    def reward_treasure(self):
+        # Determine treasure reward and provide it to the crew
+        announce("You obtain Kaptain Kragg's treasure.")
+
 
